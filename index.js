44759f4256def74f5333ee0677eaf121ed3678df
Ayuga01 <ag533329@gmail.com>
2024-09-23 20:00:16 +0530

    Message

--- index.js
+++ index.js
@@ -1,10 +1,97 @@
-const userTab = document.querySelector('data-userWeather');
-const searchTab = document.querySelector('data-searchWeather');
-const userContainer = document.querySelector('.weather-container');
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-container");
 
-const grantAccessContainer = document.querySelector('.grant-location-container');
-const searchForm = document.querySelector('data-searchForm');
-const loadingScreen = document.querySelector('.loading-container');
-const userInfoContainer = document.querySelector('.user-info-container');
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
 
 //initially variables needed
+
+let oldTab = userTab;
+const API_KEY = "b93f944f217d0a21923fca42d1d11f18";
+oldTab.classList.add("current-tab");
+
+
+function switchTab(newTab){
+    if(newTab!=oldTab){
+       oldTab.classList.remove("current-tab");
+        oldTab=newTab;
+       oldTab.classList.add("current-tab");
+
+        if(!searchForm.classList.contains("active")){
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+             searchForm.classList.add("active");
+        }
+        else{
+            searchForm.classList.remove("active");
+            userInfoContainer.classList.remove("active");
+            getfromSessionStorage();
+        }
+    }
+    
+}
+
+userTab.addEventListener("click", ()=>{
+    switchTab(userTab);
+});
+
+searchTab.addEventListener("click", ()=>{
+    switchTab(searchTab );
+});
+
+//coordinates 
+function getfromSessionStorage(){
+    const localCoordinates = sessionStorage.getItem("user-coordinates");
+    if(!localCoordinate){
+
+        grantAccessContainer.classList.add("active");
+    }
+    else{
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);
+    }
+}
+
+async function fetchUserWeatherInfo(coordinates){
+    const {lat,lon} = coordinates;
+
+    //make grant container invisible
+    grantAccessContainer.classList.remove("active");
+    //make loader availabe
+    loadingScreen.classList.add("active");
+
+    //API call
+    try{
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={API_key}&units=metric`);
+
+        const data = await res.json();
+
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+
+    }
+    catch(err){
+        loadingScreen.classList.remove("active");
+        console.log("Unable to locate user........ ERROR 404")
+    }
+    
+}
+
+function renderWeatherInfo(weatherInfo){
+    //fetch element
+    const cityName = document.querySelector("[data-cityName]");
+    const countryIcon = document.querySelector("[data-countryIcon]");
+    const desc = document.querySelector("[data-weatherDesc]");
+    const weatherIcon = document.querySelector("[data-weatherIcon]");
+    const temp = document.querySelector("[data-temp]");
+    const windspeed = document.querySelector("[data-windspeed]");
+    const humidity = document.querySelector("[data-humidity]");
+    const cloudiness = document.querySelector("[data-cloudiness]");
+
+    //fetch values from weatherInfo
+    
+}
